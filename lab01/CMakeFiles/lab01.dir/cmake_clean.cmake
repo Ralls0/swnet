3e511f1bdb074c0e4f06a36247b207d1953383a2
Ralls0 <marcomanco1998@gmail.com>
2021-10-28 13:13:36 +0000

    feat: add eth header and ip header struct

---
+++ lab01/CMakeFiles/lab01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab01.dir/lab01.o"
+  "lab01.pdb"
+  "lab01"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- lab01/lab01.c
+++ lab01/lab01.c
@@ -1,16 +1,53 @@
 #include <stdio.h>  	// Standard C 
 #include <pcap.h>	// libpcap/WinPcap
 
-#define LINE_LEN 16
+/* Ethernet addresses are 6 bytes */
+#define ETHER_ADDR_LEN	6
+/* Ethernet length */
+#define ETHER_LEN 14
 
-int main(int argc, char **argv)
-{
+/* Eth header */
+struct sniff_ethernet {
+	u_char ether_dhost[ETHER_ADDR_LEN]; /* Destination host address */
+	u_char ether_shost[ETHER_ADDR_LEN]; /* Source host address */
+	u_short ether_type; /* IP? ARP? RARP? etc */
+};
+
+/* IP address */
+struct ipaddr {
+      u_char s_b1;
+      u_char s_b2;
+      u_char s_b3;
+      u_char s_b4;
+};
+
+/* IP header */
+struct sniff_ip {
+	u_char ip_vhl;		/* version << 4 | header length >> 2 */
+	u_char ip_tos;		/* type of service */
+	u_short ip_len;		/* total length */
+	u_short ip_id;		/* identification */
+	u_short ip_off;		/* fragment offset field */
+#define IP_RF 0x8000		/* reserved fragment flag */
+#define IP_DF 0x4000		/* don't fragment flag */
+#define IP_MF 0x2000		/* more fragments flag */
+#define IP_OFFMASK 0x1fff	/* mask for fragmenting bits */
+	u_char ip_ttl;		/* time to live */
+	u_char ip_p;		/* protocol */
+	u_short ip_sum;		/* checksum */
+	struct ipaddr ip_src,ip_dst; /* source and dest address */
+};
+
+int main(int argc, char **argv) {
 	pcap_t *fp;
 	char errbuf[PCAP_ERRBUF_SIZE];
 	struct pcap_pkthdr *header;
+	struct sniff_ethernet *eth;
+	struct sniff_ip *ip;
 	const u_char *pkt_data;
 	u_int i=0;
 	int res;
+	u_int16_t ethtype;
 	pcap_if_t *alldevsp, *temp;
 	
 	if (argc != 2)
@@ -46,17 +83,38 @@ int main(int argc, char **argv)
 	/* Retrieve the packets from the file */
 	while((res = pcap_next_ex(fp, &header, &pkt_data)) >= 0)
 	{
-		/* print pkt timestamp and pkt len */
-		printf("%ld:%ld (%d)\n", header->ts.tv_sec, header->ts.tv_usec, header->len);
+		eth = (struct sniff_ethernet *) pkt_data;
+		/* print pkt timestamp */
+		fprintf(stdout, "%ld:%ld ", header->ts.tv_sec, header->ts.tv_usec);
+		/* print pkt source mac */
+		fprintf(stdout, "%02x:%02x:%02x:%02x:%02x:%02x -> ", 
+			eth->ether_shost[0], 
+			eth->ether_shost[1],
+			eth->ether_shost[2],
+			eth->ether_shost[3],
+			eth->ether_shost[4],
+			eth->ether_shost[5]
+		);
 		
-		/* Print the packet */
-		for (i=1; (i < header->caplen + 1 ) ; i++)
-		{
-			printf("%.2x ", pkt_data[i-1]);
-			if ( (i % LINE_LEN) == 0) printf("\n");
+		/* print pkt destination mac */
+		fprintf(stdout, "%02x:%02x:%02x:%02x:%02x:%02x ", 
+			eth->ether_dhost[0], 
+			eth->ether_dhost[1],
+			eth->ether_dhost[2],
+			eth->ether_dhost[3],
+			eth->ether_dhost[4],
+			eth->ether_dhost[5]
+		);
+
+		ethtype = ntohs(eth->ether_type);
+
+		if (ethtype == 0x0800) {
+			
+			ip = (struct sniff_ip *) (pkt_data + ETHER_LEN);
+			fprintf(stdout, "%d", ip->ip_src.s_b1);
+
 		}
-		
-		printf("\n\n");
+
 	}
 	
 	if (res == -1)
